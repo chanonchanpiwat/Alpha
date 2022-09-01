c795ee1b7ce6d1c4fc8be026dc5a5d44cb5efeb3
chanon.chanpiwat@gmail.com <chanon.chanpiwat@gmail.com>
2022-09-01 14:47:52 +0700

    completed

--- test/Lock.js
+++ test/Lock.js
@@ -45,10 +45,11 @@ describe('contract', function() {
       let tx = await USDTSignedByWhale.approve(Bank.address, initial_fund)
       tx = await BankSignedByWhale.deposit(initial_fund)
       console.log(`--deposit--`)
-      console.log(`Bank balance ${await USDT.balanceOf(Bank.address).then((data) => data.div(10**usdt_decimals))} usdt`)
+      const BankBalanceAfterDeposit = await USDT.balanceOf(Bank.address)
+      console.log(`Bank balance after deposit ${BankBalanceAfterDeposit.div(10**usdt_decimals)} usdt`)
 
       // expect USDT balanceOf of Bank to equal 10 million USDT after token transfer
-      expect(await USDT.balanceOf(Bank.address)).to.equal("10000000000000");
+      expect(await USDT.balanceOf(Bank.address)).to.equal(initial_fund);
     })
 
     it('Should be able lend token to the borrower with correct interest rate', async() => {
@@ -60,7 +61,6 @@ describe('contract', function() {
       const BankSignedByWhale = await Bank.connect(usdt_whale)
       const USDTSignedByWhale = await USDT.connect(usdt_whale)
 
-      
       let tx = await USDTSignedByWhale.approve(Bank.address, initial_fund)
       tx = await BankSignedByWhale.deposit(initial_fund)
 
@@ -73,21 +73,18 @@ describe('contract', function() {
       // balanceOf lender before lending the token
       const BorrowerBalanceBeforeLend = await USDT.balanceOf(owner.address)
       const LenderBalanceBeforeLend = await USDT.balanceOf(Bank.address)
-
-      
-      console.log(`Bank balance before lending ${await USDT.balanceOf(Bank.address).then((data) => data.div(10**usdt_decimals))} usdt`)
-      console.log(`Borrower balance before lending ${await USDT.balanceOf(owner.address).then((data) => data.div(10**usdt_decimals))} usdt`)
+      console.log(`Borrower balance before lending ${BorrowerBalanceBeforeLend.div(10**usdt_decimals)} usdt`)
+      console.log(`Bank balance before lending ${LenderBalanceBeforeLend.div(10**usdt_decimals)} usdt`)
 
       // lending
       tx = await BankSignedByOwner.borrow(borrowingAmount)
       console.log(`--lending--`)
 
-      console.log(`Bank balance after lending ${await USDT.balanceOf(Bank.address).then((data) => data.div(10**usdt_decimals))} usdt`)
-      console.log(`Borrower balance after lending ${await USDT.balanceOf(owner.address).then((data) => data.div(10**usdt_decimals))} usdt`)
-
-      // balanceOf lender before lending the token
+      // balanceOf lender after lending the token
       const BorrowerBalanceAfterLend = await USDT.balanceOf(owner.address)
       const LenderBalanceAfterLend = await USDT.balanceOf(Bank.address)
+      console.log(`Borrower balance after lending ${BorrowerBalanceAfterLend.div(10**usdt_decimals)} usdt`)
+      console.log(`Bank balance after lending ${LenderBalanceAfterLend.div(10**usdt_decimals)} usdt`)
 
       const totalDebt = borrowingAmount.mul(100+interest).div(100)
       // expect borrower to receive token equal borrowing amount
@@ -95,7 +92,6 @@ describe('contract', function() {
       expect(BorrowerBalanceAfterLend.sub(BorrowerBalanceBeforeLend)).to.equal(borrowingAmount)
       expect(LenderBalanceBeforeLend.sub(LenderBalanceAfterLend)).to.equal(borrowingAmount)
 
-      console.log(`Borrower balance before repay ${await USDT.balanceOf(owner.address).then((data) => data.div(10**usdt_decimals))} usdt`)
     })
 
     it("Should be able to receive a repay of borrowing fund back plus interest from borrower", async () => {
@@ -108,10 +104,12 @@ describe('contract', function() {
 
       const BankSignedByWhale = await Bank.connect(usdt_whale)
       const USDTSignedByWhale = await USDT.connect(usdt_whale)
-
       
       let tx = await USDTSignedByWhale.approve(Bank.address, initial_fund)
       tx = await BankSignedByWhale.deposit(initial_fund)
+      console.log(`--deposit--`)
+      const BankBalanceAfterDeposit = await USDT.balanceOf(Bank.address)
+      console.log(`Bank balance after deposit ${BankBalanceAfterDeposit.div(10**usdt_decimals)} usdt`)
 
       // expect USDT balanceOf of Bank to equal 10 million USDT after token transfer
       expect(await USDT.balanceOf(Bank.address)).to.equal(initial_fund);
@@ -119,15 +117,21 @@ describe('contract', function() {
       // borrower lend the token [5 million usdt] from the bank
       const BankSignedByOwner = await Bank.connect(owner)
 
-      console.log(`Bank balance before lending ${await USDT.balanceOf(Bank.address).then((data) => data.div(10**usdt_decimals))} usdt`)
-      console.log(`Borrower balance before lending ${await USDT.balanceOf(owner.address).then((data) => data.div(10**usdt_decimals))} usdt`)
+      // balanceOf lender before lending the token
+      const BorrowerBalanceBeforeLend = await USDT.balanceOf(owner.address)
+      const LenderBalanceBeforeLend = await USDT.balanceOf(Bank.address)
+      console.log(`Borrower balance before lending ${BorrowerBalanceBeforeLend.div(10**usdt_decimals)} usdt`)
+      console.log(`Bank balance before lending ${LenderBalanceBeforeLend.div(10**usdt_decimals)} usdt`)
 
       // lending
       tx = await BankSignedByOwner.borrow(borrowingAmount)
       console.log(`--lending--`)
 
-      console.log(`Bank balance after lending ${await USDT.balanceOf(Bank.address).then((data) => data.div(10**usdt_decimals))} usdt`)
-      console.log(`Borrower balance after lending ${await USDT.balanceOf(owner.address).then((data) => data.div(10**usdt_decimals))} usdt`)
+      // balanceOf lender after lending the token
+      const BorrowerBalanceAfterLend = await USDT.balanceOf(owner.address)
+      const LenderBalanceAfterLend = await USDT.balanceOf(Bank.address)
+      console.log(`Borrower balance after lending ${BorrowerBalanceAfterLend.div(10**usdt_decimals)} usdt`)
+      console.log(`Bank balance after lending ${LenderBalanceAfterLend.div(10**usdt_decimals)} usdt`)
 
       const totalDebt = borrowingAmount.mul(100+interest).div(100)
       // expect borrower to receive token equal borrowing amount
@@ -135,31 +139,30 @@ describe('contract', function() {
       expect(BorrowerBalanceAfterLend.sub(BorrowerBalanceBeforeLend)).to.equal(borrowingAmount)
       expect(LenderBalanceBeforeLend.sub(LenderBalanceAfterLend)).to.equal(borrowingAmount)
 
-
       // get current dept of borrower
       const dept = await Bank.deptOf(owner.address)
-      console.log(`Borrower balance before transfer ${await USDT.balanceOf(owner.address).then((data) => data.div(10**usdt_decimals))} usdt`)
-      console.log(`Dept of borrower ${dept.div(10**usdt_decimals)} usdt`)
+      console.log(`dept of borrower ${dept.div(10**usdt_decimals)} usdt`)
 
-      // dai whale transfer token to borrower to help paying the interest
-      await USDTSignedByWhale.transfer(owner.address, dept)
-      // borrower approving bank contract to spend their token
-      console.log(`Borrower balance after transfer ${await USDT.balanceOf(owner.address).then((data) => data.div(10**usdt_decimals))} usdt`)
-
+      // approving the token
       const USDTSignedByOwner = USDT.connect(owner)
       tx = await USDTSignedByOwner.approve(Bank.address, dept)
       
       // balanceOf lender before repaying the token
       const BorrowerBalanceBeforeRepay = await USDT.balanceOf(owner.address)
       const LenderBalanceBeforeRepay= await USDT.balanceOf(Bank.address)
-      
-      tx = await BankSignedByWhale.deposit(10)
+      console.log(`Borrower balance before repay ${BorrowerBalanceBeforeRepay.div(10**usdt_decimals)} usdt`)
+      console.log(`Bank balance before repay ${LenderBalanceBeforeRepay.div(10**usdt_decimals)} usdt`)
+
+      tx = await BankSignedByOwner.repay()
+      console.log(`--repay--`)
       
       // balanceOf lender before repaying the token
       const BorrowerBalanceAfterRepay = await USDT.balanceOf(owner.address)
       const LenderBalanceAfterRepay = await USDT.balanceOf(Bank.address)
+      console.log(`Borrower balance after repay ${BorrowerBalanceAfterRepay.div(10**usdt_decimals)} usdt`)
+      console.log(`Bank balance after repay ${LenderBalanceAfterRepay.div(10**usdt_decimals)} usdt`)
 
-      expect(BorrowerBalanceBeforeRepay.sub(BorrowerBalanceAfterRepay)).to.equal("1100000000000");
-      expect(LenderBalanceAfterRepay.sub(LenderBalanceBeforeRepay)).to.equal("1100000000000");
+      expect(BorrowerBalanceBeforeRepay.sub(BorrowerBalanceAfterRepay)).to.equal(borrowingAmount.mul(110).div(100));
+      expect(LenderBalanceAfterRepay.sub(LenderBalanceBeforeRepay)).to.equal(borrowingAmount.mul(110).div(100));
     })
   })
